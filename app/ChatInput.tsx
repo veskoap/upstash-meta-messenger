6e3acbe771a9241ea6ac065498b95c98c41440d4
Vesko <veskoap@gmail.com>
2023-01-28 19:35:18 -0500

    Integrated Redis and created addMessage api

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,11 +1,60 @@
-import React from "react";
+'use client'
+
+import { FormEvent, useState } from "react";
+import { v4 as uuid } from "uuid";
+import { Message } from "../typings";
 
 function ChatInput() {
+const [input, setInput] = useState("")
+
+const addMessage = (e: FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+
+  if (!input) return;
+
+  const messageToSend = input;
+
+  setInput("");
+
+  const id = uuid();
+
+  const message: Message = {
+    id,
+    message: messageToSend,
+    created_at: Date.now(),
+    username: 'Vesko Portev',
+    profilePic: 'https://www.vesko.me/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F1fnxaayh%2Fproduction%2Feffe9909544bbbad56837ec584ebf67c1d389021-1080x1080.jpg&w=384&q=75',
+    email: 'vesko@vesko.me'
+  }
+
+  const uploadMessageToUpstash = async () => {
+    const res = await fetch('/api/addMessage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message,
+      }),
+    });
+
+    const data = await res.json();
+  };
+
+};
+
   return (
-    <form className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-gray-100">
-      <input type="text" placeholder="Enter message here..." className="flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed"/>
+    <form onSubmit={addMessage} className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-gray-100">
+      <input 
+      type="text" 
+      value={input}
+      onChange={e => setInput(e.target.value)}
+      placeholder="Enter message here..." 
+      className="flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed"
+      />
       <button
         type="submit"
+        disabled={!input}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
       >
         Send
--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React from 'react'
+'use client';
 
 function LogoutButton() {
   return (
